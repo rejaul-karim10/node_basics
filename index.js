bde5842b563c6cc6e19f99a3cfd82e32a504ece5
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-02-09 19:17:23 +0800

    local module, export, require

---
+++ index.js
@@ -0,0 +1,7 @@
+const { getName, getAge, cgpa } = require("./students");
+
+// console.log("name:",s1.getName())
+// console.log("age:",s1.getAge())
+// console.log("result:",cgpa)
+
+console.log("name:", getName(), "age:", getAge(), "CGPA:", cgpa);
---
+++ students.js
@@ -0,0 +1,19 @@
+const getName = () => {
+  return "Rejaul Karim Rabbi";
+};
+
+const getAge = () => {
+  return "25";
+};
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge= getAge;
+// exports.result = cgpa;
+
+module.exports = {
+  getName,
+  getAge,
+  cgpa,
+};
